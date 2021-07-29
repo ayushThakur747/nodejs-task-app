30606f2476f97389824e0e4de07b91e61e2ec72f
ayushThakur747 <66708059+ayushThakur747@users.noreply.github.com>
2021-07-29 11:54:22 +0530

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,8 +2,8 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/user/user');
 const auth = async(req, res, next)=>{
     try{
-        const token = req.header('Authorization').replace('Bearer ','') //to get token from the header comming from client side
-        const decoded = jwt.verify(token,'secret'); //import jwt secret from dev.env
+        const token = req.header('Authorization').replace('Bearer ','') 
+        const decoded = jwt.verify(token,'secret'); 
         const user = await User.findOne({_id:decoded._id,'tokens.token':token});
 
         if(!user){
